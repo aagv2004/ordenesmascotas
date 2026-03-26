339737452168d74796fcea6b07960fd87bd77aa2
Alejandro Gonzalez Vergara <andresg.vergara04@gmail.com>
2026-03-26 12:39:28 -0300

    First commit

---
+++ src/main/java/com/msduoc/ordenesmascotas/OrdenesmascotasApplication.java
@@ -0,0 +1,13 @@
+package com.msduoc.ordenesmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenesmascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenesmascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msduoc/ordenesmascotas/OrdenesmascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msduoc.ordenesmascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdenesmascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
