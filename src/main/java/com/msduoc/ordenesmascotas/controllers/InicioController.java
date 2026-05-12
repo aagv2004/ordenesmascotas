e146543e31aab2276f80905764f5212f7d3a0b39
Alejandro Gonzalez Vergara <andresg.vergara04@gmail.com>
2026-05-12 15:23:50 -0400

    thymeleaf renderización de index.html

---
+++ src/main/java/com/msduoc/ordenesmascotas/controllers/InicioController.java
@@ -0,0 +1,16 @@
+package com.msduoc.ordenesmascotas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        model.addAttribute("titulo", "Bienvenido a la veterinaria");
+        return "index";
+    }
+    
+}
