5ca620c07e029e41b08c2848025894900fdadaa9
Alejandro Gonzalez Vergara <andresg.vergara04@gmail.com>
2026-05-16 15:17:50 -0400

    formularios

--- src/main/java/com/msduoc/ordenesmascotas/controllers/views/ClienteVistaController.java
+++ src/main/java/com/msduoc/ordenesmascotas/controllers/views/ClienteVistaController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.msduoc.ordenesmascotas.models.Cliente;
 import com.msduoc.ordenesmascotas.service.ClienteService;
 
 @Controller
@@ -17,5 +21,30 @@ public class ClienteVistaController {
         model.addAttribute("clientes", clienteService.getAllClientes());
         model.addAttribute("titulo", "Listado de Clientes");
         return "clientes/listadoClientes";
+    }
+
+    @GetMapping("/app/clientes/nuevo")
+    public String nuevoCliente(Model model) {
+        model.addAttribute("titulo", "Nuevo cliente");
+        model.addAttribute("cliente", new Cliente());
+        return "clientes/formClientes";
+    }
+
+    @PostMapping("/app/clientes/guardar")
+    public String guardarCliente(@ModelAttribute Cliente cliente) {
+
+        clienteService.createCliente(cliente);
+
+        return "redirect:/app/clientes";
+    }
+
+    @GetMapping("/app/clientes/editar/{id}")
+    public String editarCliente(@PathVariable Long id, Model model) {
+        Cliente cliente = clienteService.getClienteById(id).get();
+
+        model.addAttribute("titulo", "Editar cliente");
+        model.addAttribute("cliente", cliente);
+
+        return "clientes/formClientes";
     }
 }
--- src/main/java/com/msduoc/ordenesmascotas/controllers/views/OrdenVistaController.java
+++ src/main/java/com/msduoc/ordenesmascotas/controllers/views/OrdenVistaController.java
@@ -3,8 +3,15 @@ package com.msduoc.ordenesmascotas.controllers.views;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.msduoc.ordenesmascotas.models.OrdenCompra;
+import com.msduoc.ordenesmascotas.service.ClienteService;
 import com.msduoc.ordenesmascotas.service.OrdenCompraService;
+import com.msduoc.ordenesmascotas.service.ProductoService;
 import org.springframework.ui.Model;
+import com.msduoc.ordenesmascotas.enums.EstadoOrden;
 
 @Controller
 public class OrdenVistaController {
@@ -12,10 +19,52 @@ public class OrdenVistaController {
     @Autowired
     private OrdenCompraService ordenCompraService;
 
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping("/app/ordenes")
     public String listarOrdenes(Model model) {
         model.addAttribute("ordenes", ordenCompraService.getAllOrdenes());
         model.addAttribute("titulo", "Listado de Órdenes de Compra");
         return "ordenes/listadoOrdenes";
+    }
+
+    @GetMapping("/app/ordenes/nuevo")
+    public String nuevaOrden(Model model) {
+        model.addAttribute("titulo", "Nueva orden de compra");
+        model.addAttribute("estadoOrden", EstadoOrden.values());
+        model.addAttribute("orden", new OrdenCompra());
+        model.addAttribute("clientes", clienteService.getAllClientes());
+        model.addAttribute("productos", productoService.getAllProductos());
+        return "ordenes/formOrdenes";
+    }
+
+    
+    @GetMapping("/app/ordenes/editar/{id}")
+    public String editarOrden(@PathVariable Long id, Model model) {
+        OrdenCompra orden = ordenCompraService.getOrdenById(id).get();
+        
+        model.addAttribute("titulo", "Editar orden de compra");
+        model.addAttribute("estadoOrden", EstadoOrden.values());
+        model.addAttribute("orden", orden);
+        model.addAttribute("clientes", clienteService.getAllClientes());
+        model.addAttribute("productos", productoService.getAllProductos());
+        
+        return "ordenes/formOrdenes";
+    }
+    
+    @PostMapping("/app/ordenes/guardar")
+    public String guardarOrden(@ModelAttribute OrdenCompra orden) {
+
+        if (orden.getId() != null) {
+            ordenCompraService.updateOrden(orden.getId(), orden);
+        } else {
+        
+            ordenCompraService.createOrden(orden);
+        }
+        return "redirect:/app/ordenes";
     }
 }
